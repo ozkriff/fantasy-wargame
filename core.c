d3c52d83645ba6414de0c93c0eb5752dcf0affb0
ozkriff <ozkriff@gmail.com>
2011-08-21 21:41:40 +0300

    core.c: Updated terrain bonuses to melee_skill.

--- core.c
+++ core.c
@@ -75,9 +75,9 @@ static int tmvp2[5] = {  1, 2, 4, 3, 6 };
 static int tmvp3[5] = {  1, 3, 8, 4, 9 };
 
 /*terrain melee_skill bonus */
-static int tms1[5] = {  0, 0,-4, 0, 0 };
-static int tms2[5] = {  0, 3,-4, 0, 0 };
-static int tms3[5] = {  0, 1,-4, 0, 0 };
+static int tms1[5] = {  0, 1,-2, 0, 0 };
+static int tms2[5] = {  0, 2,-2, 1, 1 };
+static int tms3[5] = {  0, 1,-2, 0, 0 };
 
 
 /* GLOBAL VARIABLES */
