42b5e172f7f471a78d81fdc8934d14b4f17ef617
ozkriff <ozkriff@gmail.com>
2011-09-29 00:19:01 +0300

    utype: Added init_swordsman().

--- utype.c
+++ utype.c
@@ -124,6 +124,34 @@ init_peasant (){
   return(u);
 }
 
+static Unit_type
+init_swordsman (){
+  Unit_type u;
+  u.range_of_vision      =   3;
+  u.morale               =   5;
+  u.count                =   6;
+  u.ms                   =   5;
+  u.strength             =   4;
+  u.toughness            =   4;
+  u.attacks              =   4;
+  u.armor                =   3;
+  u.mvp                  =   3;
+  u.energy               =  20;
+  u.energy_rg            =   4;
+  u.ter_mvp[T_GRASS    ] =   1;
+  u.ter_mvp[T_FOREST   ] =   3;
+  u.ter_mvp[T_WATER    ] =   8;
+  u.ter_mvp[T_HILLS    ] =   4;
+  u.ter_mvp[T_MOUNTEENS] =   9;
+  u.ter_ms [T_GRASS    ] =   0;
+  u.ter_ms [T_FOREST   ] =   1;
+  u.ter_ms [T_WATER    ] =  -2;
+  u.ter_ms [T_HILLS    ] =   0;
+  u.ter_ms [T_MOUNTEENS] =   0;
+  u.skills_n             =   0;
+  return(u);
+}
+
 void
 init_unit_types (){
   utypes[U_DEFENDER] = init_defender();
