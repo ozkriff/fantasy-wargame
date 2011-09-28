0c1fc200a1b953b54b66bc73a1ee7aece3163a7d
ozkriff <ozkriff@gmail.com>
2011-09-29 00:12:10 +0300

    utype: Added init_peasant().

--- utype.c
+++ utype.c
@@ -96,6 +96,34 @@ init_archer (){
   return(u);
 }
 
+static void
+init_peasant (){
+  Unit_type u;
+  u.range_of_vision      =   3;
+  u.morale               =   3;
+  u.count                =   6;
+  u.ms                   =   3;
+  u.strength             =   2;
+  u.toughness            =   2;
+  u.attacks              =   3;
+  u.armor                =   1;
+  u.mvp                  =   4;
+  u.energy               =  10;
+  u.energy_rg            =   3;
+  u.ter_mvp[T_GRASS    ] =   1;
+  u.ter_mvp[T_FOREST   ] =   3;
+  u.ter_mvp[T_WATER    ] =   8;
+  u.ter_mvp[T_HILLS    ] =   4;
+  u.ter_mvp[T_MOUNTEENS] =   9;
+  u.ter_ms [T_GRASS    ] =   0;
+  u.ter_ms [T_FOREST   ] =   1;
+  u.ter_ms [T_WATER    ] =  -2;
+  u.ter_ms [T_HILLS    ] =   1;
+  u.ter_ms [T_MOUNTEENS] =   0;
+  u.skills_n             =   0;
+  return(u)
+}
+
 void
 init_unit_types (){
   utypes[U_DEFENDER] = init_defender();
