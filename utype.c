0194060d0e9ebfbda1c79991b10f6f9e6ec3bf18
ozkriff <ozkriff@gmail.com>
2011-09-29 00:22:32 +0300

    utype: Added init_scout().

--- utype.c
+++ utype.c
@@ -209,6 +209,36 @@ init_poor_archer (){
   return(u);
 }
 
+static Unit_type
+init_scout (){
+  Unit_type u;
+  u.range_of_vision      =   5;
+  u.morale               =   5;
+  u.count                =   6;
+  u.ms                   =   3;
+  u.strength             =   2;
+  u.toughness            =   3;
+  u.attacks              =   3;
+  u.armor                =   2;
+  u.mvp                  =   4;
+  u.energy               =  15;
+  u.energy_rg            =   4;
+  u.ter_mvp[T_GRASS    ] =   1;
+  u.ter_mvp[T_FOREST   ] =   3;
+  u.ter_mvp[T_WATER    ] =   8;
+  u.ter_mvp[T_HILLS    ] =   4;
+  u.ter_mvp[T_MOUNTEENS] =   9;
+  u.ter_ms [T_GRASS    ] =   0;
+  u.ter_ms [T_FOREST   ] =   1;
+  u.ter_ms [T_WATER    ] =  -2;
+  u.ter_ms [T_HILLS    ] =   0;
+  u.ter_ms [T_MOUNTEENS] =   0;
+  u.skills_n             =   2;
+  u.skills[0] = mk_skill_bool(S_IGNR);
+  u.skills[1] = mk_skill_bool(S_INVIS);
+  return(u);
+}
+
 void
 init_unit_types (){
   utypes[U_DEFENDER] = init_defender();
