3187bac6bda4c77b79eb3f909aaa7e2ef3f35bcb
ozkriff <ozkriff@gmail.com>
2011-09-30 11:23:36 +0300

    core: get_wounds(): Added int tt; /*tile type*/.

--- core.c
+++ core.c
@@ -290,12 +290,13 @@ static int
 get_wounds (Unit *a, Unit *d){
   Unit_type at = utypes[a->t];
   Unit_type dt = utypes[d->t];
+  int tt = tile(d->m)->t; /*tile type*/
   int hits     = 0;
   int wounds   = 0; /*possible wounds(may be blocked by armour)*/
   int final    = 0; /*final wounds(not blocked by armour)*/
   int attacks  = at.attacks * a->count;
-  int a_ms     = at.ms + at.ter_ms[tile(d->m)->t];
-  int d_ms     = dt.ms + dt.ter_ms[tile(d->m)->t];
+  int a_ms     = at.ms + at.ter_ms[tt];
+  int d_ms     = dt.ms + dt.ter_ms[tt];
   /*chances to hit, to wound and to ignore armour. percents.*/
   int to_hit   = 5 + (a_ms - d_ms);
   int to_wound = 5 + (at.strength - dt.toughness);
