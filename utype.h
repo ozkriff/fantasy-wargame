7ca6fbc79e975279fe4cdf5b0501265518f28a44
ozkriff <ozkriff@gmail.com>
2011-10-17 11:20:04 +0300

    utype: Added few comments, removed empty line.

--- utype.h
+++ utype.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 typedef enum {
+  /*humans*/
   U_PEASANT,
   U_MILITIAMAN,
   U_SPEARMAN,
@@ -10,15 +11,17 @@ typedef enum {
   U_POOR_ARCHER,
   U_ARCHER,
   U_SCOUT,
-
+  /*goblins*/
   U_WEAK_GOBLIN,
   U_GOBLIN,
   U_GOBLIN_SLINGER,
   U_GOBLIN_SCOUT,
+  /*orcs*/
   U_YOUNG_ORC,
   U_ORC,
   U_ARMORED_ORC,
   U_CRAZY_ORC,
+  /*trolls*/
   U_YOUNG_TROLL,
   U_TROLL
 } Unit_type_id;
